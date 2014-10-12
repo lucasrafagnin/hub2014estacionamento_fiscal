cbfd1191334e9822e2358a8c1073f770bc798103
Lucas Rafagnin <lucas_rafagnin@hotmail.com>
2014-10-12 02:26:04 -0300

    Primeiro esboço

--- src/com/mmidgard/hubestacionamento_fiscal/Inicial.java
+++ src/com/mmidgard/hubestacionamento_fiscal/Inicial.java
@@ -3,21 +3,13 @@ package com.mmidgard.hubestacionamento_fiscal;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.app.Dialog;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.Window;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.mmidgard.hubestacionamento_fiscal.models.Carro;
 
-public class Inicial extends GlobalActivity implements OnItemClickListener {
+public class Inicial extends GlobalActivity {
 
 	private AdapterListExercicios adapterExercicios;
 	private ListView listViewExercicios;
@@ -29,57 +21,30 @@ public class Inicial extends GlobalActivity implements OnItemClickListener {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.inicial);
 
-		header();
+		// header();
 
 		listaCarros = new ArrayList<Carro>();
-		criarCarrosTeste();
 		criarLista(listaCarros);
 
 		adicionarRecarga();
 	}
 
-	private void header() {
-		TextView titulo = (TextView)findViewById(R.id.titulo);
-		Button addCarro = (Button)findViewById(R.id.add_carro);
-		titulo.setText("Garagem");
-		addCarro.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				final Dialog dialog = new Dialog(Inicial.this);
-				dialog.setTitle("Adicionar carro");
-				dialog.setContentView(R.layout.add_carro);
-				dialog.setCancelable(true);
-				dialog.show();
-			}
-		});
-	}
-
-	private void criarCarrosTeste() {
-		Carro c = new Carro("abc-1234", "gol branco", true);
-		Carro c2 = new Carro("abc-1234", "gol branco", true);
-		Carro c3 = new Carro("abc-1234", "gol branco", true);
-		Carro c4 = new Carro("abc-1234", "gol branco", true);
-
-		listaCarros.add(c);
-		listaCarros.add(c2);
-		listaCarros.add(c3);
-		listaCarros.add(c4);
-	}
+	// private void criarCarrosTeste() {
+	// Carro c = new Carro("abc-1234", "gol branco", true);
+	// Carro c2 = new Carro("abc-1234", "gol branco", true);
+	// Carro c3 = new Carro("abc-1234", "gol branco", true);
+	// Carro c4 = new Carro("abc-1234", "gol branco", true);
+	//
+	// listaCarros.add(c);
+	// listaCarros.add(c2);
+	// listaCarros.add(c3);
+	// listaCarros.add(c4);
+	// }
 
 	private void criarLista(List<Carro> elementList) {
 		adapterExercicios = new AdapterListExercicios(this, listaCarros);
 
 		listViewExercicios = (ListView)findViewById(R.id.list_carros);
 		listViewExercicios.setAdapter(adapterExercicios);
-		listViewExercicios.setOnItemClickListener(this);
-	}
-
-	@Override
-	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-		Intent i = new Intent(Inicial.this, CarroSelecionado.class);
-		Carro c = (Carro)adapterExercicios.getItem(arg2);
-		i.putExtra("carro", c);
-		startActivity(i);
 	}
 }
