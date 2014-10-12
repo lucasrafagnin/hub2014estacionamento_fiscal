1c0d15b3b9ed47d774a176d56bdebe2a9c225a16
Lucas Rafagnin <lucas_rafagnin@hotmail.com>
2014-10-12 01:14:08 -0300

    personalizando header, e criando dialog do efetuar pagamento

--- src/com/mmidgard/hubestacionamento/CarroSelecionado.java
+++ src/com/mmidgard/hubestacionamento/CarroSelecionado.java
@@ -1,21 +1,33 @@
 package com.mmidgard.hubestacionamento;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.opengl.Visibility;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.mmidgard.hubestacionamento.models.Carro;
 
 public class CarroSelecionado extends Activity {
+
+	private Carro carro;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.carro_selecionado);
+
+		Intent i = getIntent();
+		Bundle b = i.getExtras();
+		carro = (Carro)b.get("carro");
+		header();
 
 		MapFragment mapFragment = (MapFragment)getFragmentManager().findFragmentById(R.id.map);
 		GoogleMap map = mapFragment.getMap();
@@ -31,6 +43,12 @@ public class CarroSelecionado extends Activity {
 
 			map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude, longitude), map.getMaxZoomLevel() - 5));
 		}
+	}
 
+	private void header() {
+		TextView titulo = (TextView)findViewById(R.id.titulo);
+		TextView add = (TextView)findViewById(R.id.add_carro);
+		titulo.setText(carro.getPlaca());
+		add.setVisibility(View.INVISIBLE);
 	}
 }
--- src/com/mmidgard/hubestacionamento/Inicial.java
+++ src/com/mmidgard/hubestacionamento/Inicial.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,7 @@ import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.mmidgard.hubestacionamento.models.Carro;
 
@@ -26,9 +28,18 @@ public class Inicial extends Activity implements OnItemClickListener {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.inicial);
 
+		header();
+
 		listaCarros = new ArrayList<Carro>();
 		criarCarrosTeste();
 		criarLista(listaCarros);
+		
+		adicionarRecarga();
+	}
+
+	private void header() {
+		TextView titulo = (TextView)findViewById(R.id.titulo);
+		titulo.setText("Garagem");
 	}
 
 	private void criarCarrosTeste() {
@@ -54,6 +65,16 @@ public class Inicial extends Activity implements OnItemClickListener {
 	@Override
 	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
 		Intent i = new Intent(Inicial.this, CarroSelecionado.class);
+		Carro c = (Carro)adapterExercicios.getItem(arg2);
+		i.putExtra("carro", c);
 		startActivity(i);
+	}
+
+	private void adicionarRecarga() {
+		final Dialog dialog = new Dialog(Inicial.this);
+		dialog.setTitle("Realizar recarga");
+		dialog.setContentView(R.layout.recarga);
+		dialog.setCancelable(true);
+		dialog.show();
 	}
 }
